1d06f64447a18c8be16bd90531af772e4b219568
anketmanoj <anket.m@live.com>
2022-02-27 14:52:58 +0400

    custom user model created

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
+
+"""Create a custom user model using BaseUserManager """
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        new_user = self.model(email=email, **extra_fields)
+        new_user.set_password(password)
+        new_user.save()
+
+        return new_user
+
+    def create_superuser(self, email, password, **extra_fields):
+        pass
