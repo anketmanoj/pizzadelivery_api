56c2a6e63755613b3691e78546ae7c09af977980
anketmanoj <anket.m@live.com>
2022-02-27 15:31:37 +0400

    customized django admin site

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Orders
 
 # Register your models here.
+
+
+@admin.register(Orders)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('size', 'customer', 'order_status',
+                    'quantity', )
+    list_filter = ('size', 'order_status', )
+    search_fields = ('customer',)
